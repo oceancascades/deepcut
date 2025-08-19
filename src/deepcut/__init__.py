f7ba8e2d99db0569a91467862236325002599c78
Jesse Cusack <jesse.cusack@oregonstate.edu>
2025-08-19 13:43:03 -0700

    add glider example and improve testing

--- src/deepcut/__init__.py
+++ src/deepcut/__init__.py
@@ -1,21 +1,4 @@
-from pathlib import Path
-
-import numpy as np
-from numpy.typing import ArrayLike
-
 from .deepcut import find_profiles
+from .example_data import get_example_data, synthetic_glider_pressure
 
-__all__ = ["find_profiles", "get_example_data"]
-
-
-def get_example_data() -> ArrayLike:
-    """
-    Load example pressure data from the tests/data directory.
-    Returns:
-        ArrayLike: 1D array of pressure values.
-    """
-    csv_path = str(
-        Path(__file__).parent.parent.parent / "tests" / "data" / "rbr_pressure.csv"
-    )
-    pressure = np.loadtxt(csv_path, delimiter=",", skiprows=2)
-    return pressure
+__all__ = ["find_profiles", "get_example_data", "synthetic_glider_pressure"]
