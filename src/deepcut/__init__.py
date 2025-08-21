1dd1de842c597cf6e6f0233eb16e77fb7d0cfc72
Jesse Cusack <jesse.cusack@oregonstate.edu>
2025-08-21 09:00:23 -0700

    working speed threshold and minimum pressure too (#4)
    * working speed threshold and minimum pressure too
    
    * remove unwanted file
    
    * remove manual line wrapping in docs

--- src/deepcut/__init__.py
+++ src/deepcut/__init__.py
@@ -1,4 +1,9 @@
-from .deepcut import find_profiles
+from .deepcut import find_profiles, find_yo
 from .example_data import get_example_data, synthetic_glider_pressure
 
-__all__ = ["find_profiles", "get_example_data", "synthetic_glider_pressure"]
+__all__ = [
+    "find_yo",
+    "find_profiles",
+    "get_example_data",
+    "synthetic_glider_pressure",
+]
