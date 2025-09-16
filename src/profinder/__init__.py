6eb43eb9b506c13ec2e688cad21ceb325314d977
Jesse Cusack <jesse.cusack@oregonstate.edu>
2025-09-16 10:07:41 -0700

    more fixes

--- src/profinder/__init__.py
+++ src/profinder/__init__.py
@@ -1,5 +1,5 @@
-from .profinder import find_profiles, find_yo
 from .example_data import get_example_data, synthetic_glider_pressure
+from .profinder import find_profiles, find_yo
 
 __all__ = [
     "find_yo",
